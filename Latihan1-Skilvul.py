9925e301979b720f440621dc097d0e3ca354f394
ajiewahyu99 <ajiewahyusofyan@gmail.com>
2023-06-13 14:09:46 +0700

    Data Latihan OKE

---
+++ Latihan1-Skilvul.py
@@ -0,0 +1,53 @@
+#Modul 1 Python Dasar
+
+##if 4 in [1,2,3,4]: print("Terdapat angka 4!")
+
+#Modul 3 Python Dasar
+
+##print('Hello',10) #Untuk mencetak huruf dan angka
+
+#Modul 5 Python Dasar
+
+##print(5) #Untuk mencetak angka
+
+#Modul 6 Python Dasar (Beragam Tipe Dasar Pada Pyton)
+
+#Beragam Tipe Data yang dapat digunakan di Python
+##print(100 * 2) # Perkalian
+##print('Hello' * 2) # Perkalian String[Huruf]
+##print('100' * 2) # Perkalian String [Angka]
+
+#Inisial Tipe Data
+##print(type(100)) # Integer
+##print(type(100.0)) # Float
+##print(type('100')) # String
+##print(type(10 + 3j)) # Complex
+
+#Ubah Tipe Data
+##print(int('10')/2) # Ubah string 10 jadi bilangan
+##print('I Like You ' + str(10)) # Ubah angka jadi string
+##a = None
+##print(type(a))
+
+#soal
+##print('halo' + str(20) * 2)
+
+#Modul 7 Python Dasar ()
+
+##print('I' + ' Love' + ' Python!') # Penambahan String pada Python
+##print('Python ' * 10) # Mencetak String Berjumlah 10
+##print('*' * 50) # Mencetak * Berjumlah 50
+##print(str(100) * 10) # Cetak Angka 100 yang sudah menjadi String Sebanyak 10
+
+##print("hello"[0]) # Pengambian Huruf Pertama
+##print("hello"[1]) # Pengambian Huruf Kedua
+##print("hello"[2]) # Pengambian Huruf Ketiga
+##print("hello"[3]) # Pengambian Huruf Keempat
+##print("hello"[4]) # Pengambian Huruf Kelima
+
+##print("hello"[-1]) # Pengambian Huruf Akhir Paling Belakang Pertama
+##print("hello"[-2]) # Pengambian Huruf Akhir Paling Belakang Kedua
+##print("hello"[-3]) # Pengambian Huruf Akhir Paling Belakang Ketiga
+##print("hello"[-4]) # Pengambian Huruf Akhir Paling Belakang Keempat
+##print("hello"[0]) # Pengambian Huruf Pertama
+
