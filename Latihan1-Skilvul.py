55cce9341e6fd2bc1cb53dba409dc797413fb141
ajiewahyu99 <ajiewahyusofyan@gmail.com>
2023-06-20 16:29:24 +0700

    Upgrade Latihan 1

--- Latihan1-Skilvul.py
+++ Latihan1-Skilvul.py
@@ -1,53 +1,151 @@
-#Modul 1 Python Dasar
+# Modul 1 Python Dasar
 
 ##if 4 in [1,2,3,4]: print("Terdapat angka 4!")
 
-#Modul 3 Python Dasar
+# Modul 3 Python Dasar
 
-##print('Hello',10) #Untuk mencetak huruf dan angka
+# print('Hello',10) #Untuk mencetak huruf dan angka
 
-#Modul 5 Python Dasar
+# Modul 5 Python Dasar
 
-##print(5) #Untuk mencetak angka
+# print(5) #Untuk mencetak angka
 
-#Modul 6 Python Dasar (Beragam Tipe Dasar Pada Pyton)
+# Modul 6 Python Dasar (Beragam Tipe Dasar Pada Pyton)
 
-#Beragam Tipe Data yang dapat digunakan di Python
-##print(100 * 2) # Perkalian
-##print('Hello' * 2) # Perkalian String[Huruf]
-##print('100' * 2) # Perkalian String [Angka]
+# Beragam Tipe Data yang dapat digunakan di Python
+# print(100 * 2) # Perkalian
+# print('Hello' * 2) # Perkalian String[Huruf]
+# print('100' * 2) # Perkalian String [Angka]
 
-#Inisial Tipe Data
-##print(type(100)) # Integer
-##print(type(100.0)) # Float
-##print(type('100')) # String
-##print(type(10 + 3j)) # Complex
+# Inisial Tipe Data
+# print(type(100)) # Integer
+# print(type(100.0)) # Float
+# print(type('100')) # String
+# print(type(10 + 3j)) # Complex
 
-#Ubah Tipe Data
-##print(int('10')/2) # Ubah string 10 jadi bilangan
-##print('I Like You ' + str(10)) # Ubah angka jadi string
+# Ubah Tipe Data
+# print(int('10')/2) # Ubah string 10 jadi bilangan
+# print('I Like You ' + str(10)) # Ubah angka jadi string
 ##a = None
-##print(type(a))
+# print(type(a))
 
-#soal
+# soal
 ##print('halo' + str(20) * 2)
 
-#Modul 7 Python Dasar ()
+# Modul 7 Python Dasar (Cara penggunaan string)
 
-##print('I' + ' Love' + ' Python!') # Penambahan String pada Python
-##print('Python ' * 10) # Mencetak String Berjumlah 10
-##print('*' * 50) # Mencetak * Berjumlah 50
-##print(str(100) * 10) # Cetak Angka 100 yang sudah menjadi String Sebanyak 10
+# print('I' + ' Love' + ' Python!') # Penambahan String pada Python
+# print('Python ' * 10) # Mencetak String Berjumlah 10
+# print('*' * 50) # Mencetak * Berjumlah 50
+# print(str(100) * 10) # Cetak Angka 100 yang sudah menjadi String Sebanyak 10
 
-##print("hello"[0]) # Pengambian Huruf Pertama
-##print("hello"[1]) # Pengambian Huruf Kedua
-##print("hello"[2]) # Pengambian Huruf Ketiga
-##print("hello"[3]) # Pengambian Huruf Keempat
-##print("hello"[4]) # Pengambian Huruf Kelima
+# print("hello"[0]) # Pengambian Huruf Pertama
+# print("hello"[1]) # Pengambian Huruf Kedua
+# print("hello"[2]) # Pengambian Huruf Ketiga
+# print("hello"[3]) # Pengambian Huruf Keempat
+# print("hello"[4]) # Pengambian Huruf Kelima
 
-##print("hello"[-1]) # Pengambian Huruf Akhir Paling Belakang Pertama
-##print("hello"[-2]) # Pengambian Huruf Akhir Paling Belakang Kedua
-##print("hello"[-3]) # Pengambian Huruf Akhir Paling Belakang Ketiga
-##print("hello"[-4]) # Pengambian Huruf Akhir Paling Belakang Keempat
-##print("hello"[0]) # Pengambian Huruf Pertama
+# print("hello"[-1]) # Pengambian Huruf Akhir Paling Belakang Pertama
+# print("hello"[-2]) # Pengambian Huruf Akhir Paling Belakang Kedua
+# print("hello"[-3]) # Pengambian Huruf Akhir Paling Belakang Ketiga
+# print("hello"[-4]) # Pengambian Huruf Akhir Paling Belakang Keempat
+# print("hello"[0]) # Pengambian Huruf Pertama
 
+# Split (Memisahkan String)
+
+##text = "Hello, nama saya, AWS"
+##x = text.split(", ")
+# print(x)
+
+# Islower (Mengecek Apa Semua Huruf Menggunakan Huruf Kecil)
+
+##a = "Halo"
+##b = "halo"
+
+# print(a.islower())
+# print(b.islower())
+
+# Count (Menghitung jumlah dari kata yg digunakan)
+
+##text = "Halo guys, hari ini kita ngebahas apa ya guys??"
+##x = text.count("Halo")
+# print(x)
+
+# print("Halo, nama saya ajie"[-1]) # Latihan
+
+
+# Modul 8 Python Dasar (Operator Basic)
+
+# print(100 + 20) # Penjumlahan
+# print(100 * 20) # Perkalian
+# print(100 - 20) # Pengurangan
+# print(100 / 20) # Pembagian
+# print(100 // 20) # Pembagian tanpa ada .0 dibelakangnya atau dibulitin hasilnya
+# print(100 % 35) # Sisa dari pembagian
+
+
+# Modul 9 Python Dasar (Prioritas Operator)
+
+# print(10+20*30)
+# print((10+20)*30)
+
+# print(5+2*3)
+##print(30*2+10 - (21 // 5))
+
+
+# Modul 10 Python Dasar (Built-in Function)
+
+# int -> mengubah angka menjadi nilai angka bilangan bulat
+##x = int("12")
+##y = int(12.5)
+
+# print(x)
+# print(y)
+
+# pow -> untuk mendapatkan nilai pangkat dari suatu angka
+# x = pow(3, 3) # 3 pangkat 3
+# y = pow(3, -3) # 3 pangkat -3
+
+# print(x)
+# print(y)
+
+
+# str -> Untuk mengubah objek menjadi string
+##x = str(123)
+##y = str(12.5)
+
+# print(type(x))
+# print(type(y))
+
+
+# Modul 11 Python Dasar (Variabel)
+
+# Variabel untuk menampung data
+# Contoh
+
+##berat = 78.7
+##manusia = ('David', 78.7)
+
+# print(berat)
+# print(manusia)
+
+
+# Modul 12 Python Dasar (Deklarasi Variabel)
+
+# lebar = 10
+# tinggi = 5
+# luas_kotak = lebar * tinggi
+# print(luas_kotak)
+
+# Modul 13 Python Dasar (Variabel Assignment)
+
+# Simultaneous Assignment
+##x, y = 100, 200
+##result = x + y
+# print(result)
+
+# Multiple Assignment
+##num1 = num2 = num3 = 300
+##print(num1, num2, num3)
+
+# Modul 14 Python Dasar (Compound Assignment Operators)
